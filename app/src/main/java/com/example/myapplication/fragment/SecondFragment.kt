f0a19d4ec2b77bb8217b29819564baf72b3a3972
student <student@BTU.local>
2023-04-19 16:19:03 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/fragment/SecondFragment.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.fragment
+
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class SecondFragment : Fragment(R.layout.fragment_second) {
+}
